a784896973489897fbd015758c30211c5da2ea8e
joris hermans <JHermans85@hotmail.com>
2013-12-23 11:27:42 +0100

    adding new code for new force version

--- bin/server.dart
+++ bin/server.dart
@@ -16,7 +16,7 @@ void main() {
     print('${rec.level.name}: ${rec.time}: ${rec.message}');
   });
   
-  ForceServer fs = new ForceServer(wsPath: "/ws", bind_address: InternetAddress.ANY_IP_V4, port: 9223, startPage: "forcechat.html" );
+  ForceServer fs = new ForceServer(wsPath: "/ws", host: InternetAddress.ANY_IP_V4, port: 9223, startPage: "forcechat.html" );
   
   fs.on('text', (e, sendable) { 
     var json = e.json;
